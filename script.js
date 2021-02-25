666aed1c10f2c6c8a6fb3354119ebf3816c57f98
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-02-25 10:48:09 -0500

    troubleshooting characterSearch function in script.js - can't get fields to dynamically generate in column on html

--- script.js
+++ script.js
@@ -38,3 +38,63 @@ $(".searchBtn1").on("click", function (event) {
   event.preventDefault();
   searchGame();
 });
+
+//Function to query for character
+function searchCharacter() {
+  const characterInput = $("#characterinput").val();
+  console.log(characterInput);
+
+  const characterQueryURL =
+    "https://zelda-api.apius.cc/api/characters?name=" + characterInput;
+
+  $.ajax({
+    url: characterQueryURL,
+    method: "GET",
+  }).then(function (response) {
+      console.log(response);
+      $("#characterInfoView").empty();
+      for (var i = 0; i < data.length; i++) {
+          console.log(data[i]);
+          
+        //   const dataItem = data[i];
+          const column = $("<div>").addClass("col-sm-8");
+          const card = $("<div>").addClass("card");
+          const characterNameEl = $("<h3>")
+            .addClass(".characterName")
+            .text("Name: " + response.data[i].name);
+
+          const charDescriptionEl = $("<p>")
+            .addClass(".char_description")
+            .text("Description: " + response.data[i].description);
+
+          const genderEl = $("<p>")
+            .addClass(".gender")
+            .text("Gender: " + response.data[i].gender);
+
+          const raceEl = $("<p>")
+            .addClass(".race")
+            .text("Race: " + response.data[i].race);
+
+          const appearancesEl = $("<p>")
+            .addClass(".appearances")
+              .text("Appearances: " + response.data[i].appearances);
+          
+          card.append(characterNameEl);
+          card.append(genderEl);
+          card.append(raceEl);
+          card.append(appearancesEl);
+          card.append(charDescriptionEl);
+          column.append(card);
+
+          $("#characterInfoView").append(column);
+          
+      }
+  });
+}
+
+// Create add event listener for character search button when clicked
+
+$(".searchBtn2").on("click", function (event) {
+  event.preventDefault();
+  searchCharacter();
+});
